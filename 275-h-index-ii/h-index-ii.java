3154a52e11e77a247be8921652621411ddb006a0
Sudharshana-05 <sudharshana2k2@gmail.com>
2025-06-09 14:33:10 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (79.97%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int n=citations.length;
+            int low=0,high=n;
+            
+            while(low<high){
+               int mid=low+(high-low)/2;
+            if(citations[mid]>=n-mid){
+                high=mid;
+            }    
+            else{
+                low=mid+1;
+            }
+    }
+    return n-low;
+    }
+}
