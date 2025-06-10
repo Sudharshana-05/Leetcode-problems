9772451cc4cc26fd494c0d6dc6d43aab57343615
Sudharshana-05 <sudharshana2k2@gmail.com>
2025-06-10 14:55:41 +0530

    Time: 25 ms (70.25%) | Memory: 40.4 MB (50.87%) - LeetSync

--- 278-first-bad-version/first-bad-version.java
+++ 278-first-bad-version/first-bad-version.java
@@ -6,7 +6,6 @@ public class Solution extends VersionControl {
         int low=1,high=n;
         while(low<=high){
             int mid=low+(high-low)/2;
-        
             if(isBadVersion(mid)==false){
                 low=mid+1;
             }
