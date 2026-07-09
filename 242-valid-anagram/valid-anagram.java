e04649d5dd23484089df61072a695917db37f216
Sudharshana-05 <sudharshana2k2@gmail.com>
2026-07-09 14:32:28 +0530

    Time: 14 ms (26.46%) | Memory: 46.6 MB (16.25%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,3 +1,24 @@
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if(s.length()!=t.length())
+//         return false;
+//         HashMap<Character,Integer>h1=new HashMap<>();
+//         for(char c:s.toCharArray()){
+//             h1.put(c,h1.getOrDefault(c,0)+1);
+//         }
+//         HashMap<Character,Integer>h2=new HashMap<>();
+//         for(char c:t.toCharArray()){
+//             h2.put(c,h2.getOrDefault(c,0)+1);
+//         }
+//         for(char c:s.toCharArray()){
+//             if(!h1.get(c).equals(h2.get(c)))
+//             return false;
+//         }
+//         return true;
+
+//     }
+// }
+
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length())
@@ -6,12 +27,12 @@ class Solution {
         for(char c:s.toCharArray()){
             h1.put(c,h1.getOrDefault(c,0)+1);
         }
-        HashMap<Character,Integer>h2=new HashMap<>();
+        
         for(char c:t.toCharArray()){
-            h2.put(c,h2.getOrDefault(c,0)+1);
+            h1.put(c,h1.getOrDefault(c,0)-1);
         }
-        for(char c:s.toCharArray()){
-            if(!h1.get(c).equals(h2.get(c)))
+        for(int freq: h1.values()){
+            if(freq!=0)
             return false;
         }
         return true;
