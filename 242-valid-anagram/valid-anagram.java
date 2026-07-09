42464891d26a637b873703835a5eba830898750a
Sudharshana-05 <sudharshana2k2@gmail.com>
2026-07-09 14:23:39 +0530

    Time: 15 ms (23.97%) | Memory: 46.7 MB (16.25%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())
+        return false;
+        HashMap<Character,Integer>h1=new HashMap<>();
+        for(char c:s.toCharArray()){
+            h1.put(c,h1.getOrDefault(c,0)+1);
+        }
+        HashMap<Character,Integer>h2=new HashMap<>();
+        for(char c:t.toCharArray()){
+            h2.put(c,h2.getOrDefault(c,0)+1);
+        }
+        for(char c:s.toCharArray()){
+            if(!h1.get(c).equals(h2.get(c)))
+            return false;
+        }
+        return true;
+
+    }
+}
