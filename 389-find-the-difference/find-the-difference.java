fe33aff0aa3167e01d868d921d95ffdd7cc006c4
Sudharshana-05 <sudharshana2k2@gmail.com>
2026-07-10 16:43:55 +0530

    Time: 2 ms (66.62%) | Memory: 43.3 MB (35.53%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.java
@@ -0,0 +1,22 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+       
+        int[]freq=new int[26];
+        for(int i:s.toCharArray()){
+            freq[i-'a']++;
+
+        }
+        for(int i:t.toCharArray()){
+             freq[i-'a']--;
+        }
+        for(int i=0;i<26;i++){
+             if(freq[i]==-1){
+                return (char)(i+'a');
+             }
+        }
+        return ' ';
+            
+        
+
+    }
+}
