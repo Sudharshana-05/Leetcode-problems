dfb5e3fe8c82347570597d754225abf1622fd1e3
Sudharshana-05 <sudharshana2k2@gmail.com>
2026-07-10 11:34:21 +0530

    Time: 2 ms (70.26%) | Memory: 55.6 MB (59.64%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,16 +1,38 @@
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         int n=nums.length;
+//         HashMap<Integer,Integer>mp=new HashMap<>();
+//         for(int arr:nums){
+//             mp.put(arr,mp.getOrDefault(arr,0)+1);
+//         }
+//         for(Map.Entry<Integer,Integer>entry:mp.entrySet()){
+//             if(entry.getValue()>n/2){
+//                 return entry.getKey();
+//             }
+//         }
+//         return -1;
+
+//     }
+// }
+
 class Solution {
     public int majorityElement(int[] nums) {
-        int n=nums.length;
-        HashMap<Integer,Integer>mp=new HashMap<>();
-        for(int arr:nums){
-            mp.put(arr,mp.getOrDefault(arr,0)+1);
-        }
-        for(Map.Entry<Integer,Integer>entry:mp.entrySet()){
-            if(entry.getValue()>n/2){
-                return entry.getKey();
+        int cnt=0;
+        int candidate=0;
+        for(int i:nums){
+            if(cnt==0){
+                candidate=i;
+            }
+            if(i==candidate){
+                cnt++;
+
+
+            }
+            else{
+                cnt--;
             }
         }
-        return -1;
+        return candidate;
 
     }
 }
