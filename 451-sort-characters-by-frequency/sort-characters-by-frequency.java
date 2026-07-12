8b76bd35b9c2142c5c4dc448b9b4673f74e73bdf
Sudharshana-05 <sudharshana2k2@gmail.com>
2026-07-12 11:58:55 +0530

    Time: 12 ms (86.61%) | Memory: 47.5 MB (15.40%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,26 +1,55 @@
-class Solution {
+// class Solution {
+//     public String frequencySort(String s) {
+//         HashMap<Character,Integer>mp=new HashMap<>();
+//         for(char ch:s.toCharArray()){
+//             mp.put(ch,mp.getOrDefault(ch,0)+1);
+//         }
+//         //using Priority Queue to implement the maxheap
+//         PriorityQueue<Character>pq=new PriorityQueue<>(
+//             (a,b) -> mp.get(b)-mp.get(a)
+//         );
+//         pq.addAll(mp.keySet());
+
+//         StringBuilder sb=new StringBuilder();
+//         while(!pq.isEmpty()){
+//             char c=pq.poll();
+//             int cnt=mp.get(c);
+        
+//         while(cnt-->0){
+//             sb.append(c);
+//         }
+//         }
+//         return sb.toString();
+
+
+//     }
+// }
+
+class Solution{
     public String frequencySort(String s) {
         HashMap<Character,Integer>mp=new HashMap<>();
         for(char ch:s.toCharArray()){
             mp.put(ch,mp.getOrDefault(ch,0)+1);
         }
-        //using Priority Queue to implement the maxheap
-        PriorityQueue<Character>pq=new PriorityQueue<>(
-            (a,b) -> mp.get(b)-mp.get(a)
-        );
-        pq.addAll(mp.keySet());
+        ArrayList<Character>[]bucket=new ArrayList[s.length()+1];
 
-        StringBuilder sb=new StringBuilder();
-        while(!pq.isEmpty()){
-            char c=pq.poll();
-            int cnt=mp.get(c);
-        
-        while(cnt-->0){
-            sb.append(c);
+        for(char c:mp.keySet()){
+            int f=mp.get(c);
+            if(bucket[f]==null){
+                bucket[f]=new ArrayList<>();
+            }
+            bucket[f].add(c);
         }
+        StringBuilder sb=new StringBuilder();
+        for(int i=bucket.length-1;i>=0;i--){
+            if(bucket[i]!=null){
+                for(char c:bucket[i]){
+                    for(int j=0;j<i;j++){
+                        sb.append(c);
+                    }
+                }
+            }
         }
         return sb.toString();
-
-
     }
 }
