26af71b50724d9579ee5c3c30978df1b8911d36d
Sudharshana-05 <sudharshana2k2@gmail.com>
2026-08-27 22:24:49 +0530

    Time: 8 ms (95.74%) | Memory: 46.9 MB (58.44%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,57 +1,49 @@
-// brute force Approach
-//convert p into characters
-// sort p
-//take the substring of s
-//convert the substring into characters
-// sort the substring
-//check whether sorted p and sorted substring are same,if yes,add it in the list
-// class Solution {
-//     public List<Integer> findAnagrams(String s, String p) {
-//     List<Integer>ans=new ArrayList<>();
-//     int n=s.length();
-//     int m=p.length();
-//     char []pArr=p.toCharArray();
-//     Arrays.sort(pArr);
-//     for(int i=0;i<=n-m;i++){
-//         String sub=s.substring(i,i+m);
-//         char []sArr=sub.toCharArray();
-//         Arrays.sort(sArr);
-    
-//     if(Arrays.equals(pArr,sArr)){
-//         ans.add(i);
-//     }}
-//     return ans;
-//     }
-// }
 
-//Better Approach --> Fixed Sliding Window+ Frequency
-//Instead of sorting,we can calculate its frequency
+
+//Optimal Approach --> Fixed Sliding Window + 1Frequency Array + Counting 
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
     List<Integer>ans=new ArrayList<>();
 
-    int[]pFreq=new int[26];
-    int []wFreq=new int[26];
+    int[]Freq=new int[26];
+  
     int n=s.length();
     int m=p.length();
     if(n<m)
     return ans;
     for(char ch:p.toCharArray()){
-      pFreq[ch-'a']++;
+      Freq[ch-'a']++;
     }
+    int cnt=0;
     for(int i=0;i<m;i++){
-        wFreq[s.charAt(i)-'a']++;
+        int index=s.charAt(i)-'a';
+        if(Freq[index]>0){
+           cnt++;
+        }
+        Freq[index]--;
+
     }
-    if(Arrays.equals(wFreq,pFreq)){
+    if(cnt==m){
         ans.add(0);
     }
+   
     for(int i=m;i<n;i++){
-        wFreq[s.charAt(i)-'a']++;
-        wFreq[s.charAt(i-m)-'a']--;
-        if(Arrays.equals(wFreq,pFreq)){
+        int addIndex=s.charAt(i)-'a';
+        if(Freq[addIndex]>0){
+            cnt++;
+        }
+        Freq[addIndex]--;
+        int remove=s.charAt(i-m)-'a';
+        if(Freq[remove]>=0){
+            cnt--;
+        }
+        Freq[remove]++;
+        if(cnt==m){
             ans.add(i-m+1);
         }
-    }
+        
+    
+}
     return ans;
 
     }
