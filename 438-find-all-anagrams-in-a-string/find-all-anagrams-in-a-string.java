aa9b417bb1e96e153090ef3a3204439f0464f07b
Sudharshana-05 <sudharshana2k2@gmail.com>
2026-08-27 22:11:25 +0530

    Time: 9 ms (92.39%) | Memory: 47.1 MB (36.09%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -5,21 +5,54 @@
 //convert the substring into characters
 // sort the substring
 //check whether sorted p and sorted substring are same,if yes,add it in the list
+// class Solution {
+//     public List<Integer> findAnagrams(String s, String p) {
+//     List<Integer>ans=new ArrayList<>();
+//     int n=s.length();
+//     int m=p.length();
+//     char []pArr=p.toCharArray();
+//     Arrays.sort(pArr);
+//     for(int i=0;i<=n-m;i++){
+//         String sub=s.substring(i,i+m);
+//         char []sArr=sub.toCharArray();
+//         Arrays.sort(sArr);
+    
+//     if(Arrays.equals(pArr,sArr)){
+//         ans.add(i);
+//     }}
+//     return ans;
+//     }
+// }
+
+//Better Approach --> Fixed Sliding Window+ Frequency
+//Instead of sorting,we can calculate its frequency
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
     List<Integer>ans=new ArrayList<>();
+
+    int[]pFreq=new int[26];
+    int []wFreq=new int[26];
     int n=s.length();
     int m=p.length();
-    char []pArr=p.toCharArray();
-    Arrays.sort(pArr);
-    for(int i=0;i<=n-m;i++){
-        String sub=s.substring(i,i+m);
-        char []sArr=sub.toCharArray();
-        Arrays.sort(sArr);
-    
-    if(Arrays.equals(pArr,sArr)){
-        ans.add(i);
-    }}
+    if(n<m)
     return ans;
+    for(char ch:p.toCharArray()){
+      pFreq[ch-'a']++;
+    }
+    for(int i=0;i<m;i++){
+        wFreq[s.charAt(i)-'a']++;
+    }
+    if(Arrays.equals(wFreq,pFreq)){
+        ans.add(0);
+    }
+    for(int i=m;i<n;i++){
+        wFreq[s.charAt(i)-'a']++;
+        wFreq[s.charAt(i-m)-'a']--;
+        if(Arrays.equals(wFreq,pFreq)){
+            ans.add(i-m+1);
+        }
+    }
+    return ans;
+
     }
 }
