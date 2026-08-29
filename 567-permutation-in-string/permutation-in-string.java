27120f4a47ac5dbee5f1bb0b364fd0aa2ff64ec7
Sudharshana-05 <sudharshana2k2@gmail.com>
2026-08-29 18:00:15 +0530

    Time: 5 ms (97.42%) | Memory: 43.6 MB (98.48%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,20 +1,30 @@
+//Better Approach [Fixed Sliding Window + 2 Frequency array]
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         int n=s1.length();
         int m=s2.length();
-        
-        char []s1Arr=s1.toCharArray();
-        Arrays.sort(s1Arr);
-        for(int i=0;i<=m-n;i++){
-            String sub=s2.substring(i,i+n);
-            char[]s2Arr=sub.toCharArray();
-            Arrays.sort(s2Arr);
-            if(Arrays.equals(s1Arr,s2Arr)){
+        int[] s1Freq=new int[26];
+        int[] s2Freq=new int[26];
+        if(n>m){
+            return false;
+        }
+        for(char ch:s1.toCharArray()){
+            s1Freq[ch-'a']++;
+        }
+        for(int i=0;i<n;i++){
+            s2Freq[s2.charAt(i)-'a']++;
+        }
+        if(Arrays.equals(s1Freq,s2Freq)){
             return true;
         }
-
+        for(int i=n;i<m;i++){
+            s2Freq[s2.charAt(i)-'a']++;
+            s2Freq[s2.charAt(i-n)-'a']--;
+            if(Arrays.equals(s1Freq,s2Freq)){
+                return true;
+            }
         }
-        
+
         return false;
     }
 }
